9099702b15ae0cb53c7d43a179a24ced751f30ff
Junfeng Hu <junfeng.hu@screeningeagle.com>
2022-09-14 14:25:00 +0800

    Update Package.swift for v3.6.0-xcode14-fix

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Charts",
-            url: "https://github.com/screeningeagledreamlab/ChartsXCFramework/releases/download/v3.6.0/Charts.xcframework.zip",
-            checksum: "a19b330c8d2cf79d668094ab06c3f2721b47188724c78095fc9fc22a876a4ed8"
+            url: "https://github.com/screeningeagledreamlab/ChartsXCFramework/releases/download/v3.6.0-xcode14-fix/Charts.xcframework.zip",
+            checksum: "a8b8c452a331fb3b93d536058fd0413e2a1493698c032a5cfb2a99ec5584c92d"
         )
     ]
 )
