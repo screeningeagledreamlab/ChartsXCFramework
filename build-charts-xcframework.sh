2ea65b13f4514e6588177657cd0abea23acf8926
John Goh <john.goh@screeningeagle.com>
2023-04-13 18:00:46 +0800

    Update Package.swift for 3.6.0-catalyst

--- build-charts-xcframework.sh
+++ build-charts-xcframework.sh
@@ -15,7 +15,16 @@ xcodebuild archive \
     -archivePath "build/Charts-iOS.xcarchive" \
     SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
+xcodebuild archive \
+    -project "Charts.xcodeproj" \
+    -scheme "Charts" \
+    -configuration Release \
+    -destination "generic/platform=macOS,variant=Mac Catalyst" \
+    -archivePath "build/Charts-Catalyst.xcarchive" \
+    SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
 xcodebuild -create-xcframework \
     -framework build/Charts-iOS.xcarchive/Products/Library/Frameworks/Charts.framework \
     -framework build/Charts-iOSSimulator.xcarchive/Products/Library/Frameworks/Charts.framework \
+    -framework build/Charts-Catalyst.xcarchive/Products/Library/Frameworks/Charts.framework \
     -output build/Charts.xcframework
